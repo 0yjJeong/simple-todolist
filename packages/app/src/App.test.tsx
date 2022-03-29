5ce17a7dc865a7f8d1a45fbabfdca4e929a39dc3
sinijeong <sinijeong.only@gmail.com>
2022-03-29 12:37:24 +0900

    app: routing

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -3,9 +3,28 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('renders', () => {
+  it("should be rendered as '/'", () => {
     render(<App />);
+    screen.getByText('Home');
+  });
+
+  it("should be rendered as '/login'", () => {
+    window.history.pushState({}, '', '/login');
+    render(<App />);
+    screen.getByText('Login');
+  });
+
+  it('should render a todolist', () => {
+    window.history.pushState({}, '', '/a/todos');
+    render(<App />);
+    screen.getByText('User');
     screen.getByText('a');
     screen.getByText('b');
+  });
+
+  it('should land on a not found page', () => {
+    window.history.pushState({}, '', '/a/b');
+    render(<App />);
+    screen.getByText(/Not found/i);
   });
 });
--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
+import GlobalStyles from './GlobalStyles';
 import TodoList from '@todolist/plugin-todo';
 
 const todos = [
-  { id: 'a', title: 'a', checked: true },
-  { id: 'b', title: 'b', checked: false },
+  {
+    id: 'a',
+    title: 'a',
+    checked: true,
+  },
+  {
+    id: 'b',
+    title: 'b',
+    checked: false,
+  },
 ];
 
 function App() {
-  return <TodoList todos={todos} />;
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <Routes>
+        <Route path='/'>
+          <Route index element={<>Home</>} />
+          <Route path='login' element={<>Login</>} />
+          <Route path='signup' element={<>Signup</>} />
+        </Route>
+        <Route
+          path=':user'
+          element={
+            <>
+              User
+              <Outlet />
+            </>
+          }
+        >
+          <Route path='todos' element={<TodoList todos={todos} />} />
+          <Route path='*' element={<>Not found</>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
