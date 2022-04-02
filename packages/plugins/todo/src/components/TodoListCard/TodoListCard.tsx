4096995ceed705ce66e508218c6b6d6935a6a7a7
sinijeong <sinijeong.only@gmail.com>
2022-04-02 13:56:40 +0900

    todo: added card delete button

--- packages/plugins/todo/src/components/TodoListCard/TodoListCard.tsx
+++ packages/plugins/todo/src/components/TodoListCard/TodoListCard.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styled from 'styled-components';
+import { MdRemove } from 'react-icons/md';
 import { Todo } from '../../types';
 import CheckButton from './TodoListCardCheckButton';
 
@@ -8,10 +9,14 @@ type TodoListCardProps = {
 };
 
 const TodoListCard: FC<TodoListCardProps> = ({ todo }) => {
+  const [clicked, setClicked] = useState(false);
   return (
-    <Card checked={todo.checked}>
+    <Card checked={todo.checked} onClick={() => setClicked(!clicked)}>
       <CheckButton checked={todo.checked} />
       <p>{todo.title}</p>
+      <CardRemoveButton active={clicked}>
+        <MdRemove /> 삭제하기
+      </CardRemoveButton>
     </Card>
   );
 };
@@ -27,6 +32,8 @@ const Card = styled.li<{ checked: boolean }>`
   padding-left: 1rem;
   padding-top: 1rem;
   padding-bottom: 1rem;
+  position: relative;
+  overflow: hidden;
 
   ${(props) =>
     props.checked &&
@@ -43,4 +50,27 @@ const Card = styled.li<{ checked: boolean }>`
   & + & {
     margin-top: 0.4rem;
   }
+`;
+
+const CardRemoveButton = styled.button<{ active: boolean }>`
+  border: none;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: -90px;
+  background: #ffb8b8;
+  color: #f26d6d;
+  width: 90px;
+  display: flex;
+  gap: 0.2rem;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  transition: 0.4s;
+
+  ${(props) =>
+    props.active &&
+    `
+  right: 0;
+  `}
 `;
