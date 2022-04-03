02c088e83cfd9cf039a3cb7890ea7c9fac9da60f
sinijeong <sinijeong.only@gmail.com>
2022-04-03 11:35:11 +0900

    test(todo): update test code

--- packages/plugins/todo/src/components/TodoListCard/TodoListCard.test.tsx
+++ packages/plugins/todo/src/components/TodoListCard/TodoListCard.test.tsx
@@ -3,19 +3,25 @@ import { render, fireEvent } from '@testing-library/react';
 import TodoListCard from './TodoListCard';
 
 describe('<TodoListCard/>', () => {
-  const todo = {
+  const checkedTodo = {
     id: 'a',
     title: 'a',
+    checked: true,
+  };
+
+  const uncheckedTodo = {
+    id: 'b',
+    title: 'b',
     checked: false,
   };
 
   it('should render', () => {
-    const rendered = render(<TodoListCard todo={todo} />);
+    const rendered = render(<TodoListCard todo={checkedTodo} />);
     expect(rendered.getByText('a')).toBeInTheDocument();
   });
 
   it('should render delete button', () => {
-    const rendered = render(<TodoListCard todo={todo} />);
+    const rendered = render(<TodoListCard todo={checkedTodo} />);
     fireEvent(
       rendered.container.querySelector('li'),
       new MouseEvent('click', { bubbles: true, cancelable: true })
@@ -24,5 +30,29 @@ describe('<TodoListCard/>', () => {
     expect(
       removeButton.classList.contains('todolist__card-removebtn-active')
     ).toBe(true);
+  });
+
+  it('should render check icon', () => {
+    const rendered = render(<TodoListCard todo={checkedTodo} />);
+    const checkbox =
+      rendered.container.getElementsByClassName('todolist__checkbox')[0];
+    expect(checkbox).toBeTruthy();
+    fireEvent(
+      checkbox,
+      new MouseEvent('click', { bubbles: true, cancelable: true })
+    );
+    expect(checkbox.classList.contains('todolist__checkbox-checked')).toBe(
+      true
+    );
+  });
+
+  it('should not render check icon', () => {
+    const rendered = render(<TodoListCard todo={uncheckedTodo} />);
+    const checkbox =
+      rendered.container.getElementsByClassName('todolist__checkbox')[0];
+    expect(checkbox).toBeTruthy();
+    expect(checkbox.classList.contains('todolist__checkbox-checked')).toBe(
+      false
+    );
   });
 });
