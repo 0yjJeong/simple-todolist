1912f8bec5e43554734f71593eeb5e00bb6214a0
sinijeong <sinijeong.only@gmail.com>
2022-04-10 23:04:53 +0900

    todo/CheckBox: foward checkbox component to props

--- packages/plugins/todo/src/components/CheckBox/CheckBox.tsx
+++ packages/plugins/todo/src/components/CheckBox/CheckBox.tsx
@@ -2,16 +2,16 @@ import React, { FC } from 'react';
 import { useCheck } from './CheckProvider';
 
 type Props = {
+  Component?: React.FunctionComponent<React.HTMLProps<HTMLInputElement>>;
   onCheck?: React.ChangeEventHandler<HTMLInputElement>;
 };
 
-const CheckBox: FC<Props> = ({ onCheck }) => {
+const CheckBox: FC<Props> = ({
+  Component = (props) => <input {...props} />,
+  onCheck,
+}) => {
   const checked = useCheck();
-  return (
-    <label>
-      <input type='checkbox' checked={checked} onChange={onCheck} />
-    </label>
-  );
+  return <Component type='checkbox' checked={checked} onChange={onCheck} />;
 };
 
 export default CheckBox;
