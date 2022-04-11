eb79cdd00f6c4f76c34e013c8dc21d89f6fc4fdd
sinijeong <sinijeong.only@gmail.com>
2022-04-11 19:01:10 +0900

    todo: remove card-related

--- packages/plugins/todo/src/components/Cards/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as BaseCard } from './BaseCard';
-export { default as DefaultCard } from './DefaultCard';
--- packages/plugins/todo/src/index.ts
+++ packages/plugins/todo/src/index.ts
@@ -1,7 +1,6 @@
 export * from './types';
 export * from './hooks';
 
-export * from './components/Cards';
 export { default } from './components/TodoList/TodoList';
 export { default as TodoListForm } from './components/TodoListForm/TodoListForm';
 export { default as CheckBox } from './components/CheckBox/CheckBox';
